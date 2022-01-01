d65a273941c50c8a5629bb785eee01cd6e0acd64
shayzaran <shayzaran@gmail.com>
2022-01-01 18:03:09 +0300

    Forms with formik, validation with Yup

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -1,19 +1,41 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "../utils/Button";
+import * as Yup from "yup";
+import TextField from "../forms/TextField";
 
 export default function CreateGenre() {
-  const history = useHistory();
   return (
     <>
       <h3>Create Genre</h3>
-      <Button
-        onClick={() => {
-          history.push("/genres");
+
+      <Formik
+        initialValues={{
+          name: "",
         }}
+        onSubmit={async (value) => {
+          await new Promise((r) => setTimeout(r, 1));
+          console.log(value);
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string()
+            .required("This field is required")
+            .firsLetterUppercase(),
+        })}
       >
-        Save Changes
-      </Button>
+        {(formikProps) => (
+          <Form>
+            <TextField field="name" displayName="Name"></TextField>
+            <Button disabled={formikProps.isSubmitting} type="submit">
+              Save Changes
+            </Button>
+            <Link className="btn btn-secondary" to="/genres">
+              Cancel
+            </Link>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -2,14 +2,25 @@ import React from "react";
 
 export default function Button(props: buttonProps) {
   return (
-    <button className="btn btn-primary" onClick={props.onClick}>
-      {" "}
-      {props.children}{" "}
+    <button
+      type={props.type}
+      className="btn btn-primary"
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
     </button>
   );
 }
 
 interface buttonProps {
   children: React.ReactNode;
-  onClick(): void;
+  onClick?(): void;
+  type: "button" | "submit";
+  disabled: boolean;
 }
+
+Button.defaultProps = {
+  type: "button",
+  disabled: false,
+};
